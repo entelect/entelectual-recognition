ba0ce7bf88f8484039c4babe369de2389f6e6032
Jandre' Coetzee <jandre.coetzee@entelect.co.za>
2019-09-29 12:42:59 +0200

    Delete attendee from api

--- entelectual-recognition-api/routes/attendee.js
+++ entelectual-recognition-api/routes/attendee.js
@@ -11,28 +11,27 @@ const models = require("../database/models");
 *     name: Attendee List
 *     summary: Gets list of attendees by event
 *     parameters:
-*       - in: query
-*         name: eventId
-*         schema:
-*           type: integer
-*         required:
-*           - eventId
+ *       - name: eventId
+ *         description: Event Id
+ *         in: query
+ *         required: true
+ *         type: integer
 *     produces:
 *       - application/json
 *     responses:
 *       200:
 *         description: List of attendees as a json object 
 */
-router.get('/', async function(req, res, next) {
-    try {
-      const { eventId } = req.query;
-      const attendees = await models.Attendee.findAll({
-        where: { eventId: eventId }
-      });
-      return res.status(200).json({ attendees });
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
+router.get('/', async function (req, res, next) {
+  try {
+    const { eventId } = req.query;
+    const attendees = await models.Attendee.findAll({
+      where: { eventId: eventId }
+    });
+    return res.status(200).json({ attendees });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 
@@ -65,18 +64,65 @@ router.get('/', async function(req, res, next) {
 *       200:
 *         description: List of attendees as a json object 
 */
-router.post('/add', async function(req,res){
+router.post('/add', async function (req, res) {
 
-    try {
-        const { username, eventId } = req.body;
-        const atendee = await models.Attendee.create({
-            username: username,
-            eventId: eventId,
-        })
-        return res.status(200).json({ atendee });
-      } catch (error) {
-        return res.status(500).send(error.message);
+  try {
+    const { username, eventId } = req.body;
+    const atendee = await models.Attendee.create({
+      username: username,
+      eventId: eventId,
+    })
+    return res.status(200).json({ atendee });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+
+
+/**
+* @swagger
+* /v1/attendee/delete:
+ *   post:
+ *     tags:
+ *       - attendees
+ *     description: Deletes a single attendee
+ *     produces:
+ *       - application/json
+*     parameters:
+*       - name: body
+*         in: body
+*         schema:
+*           type: object
+*           properties:
+*             username:
+*               type: string
+*             eventId:
+*               type: integer
+*         required:
+*           - username
+*           - eventId
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+*/
+router.post('/delete', async function (req, res) {
+  try {
+    const { eventId, username } = req.body;
+    const deleted = await models.Attendee.destroy({
+      where: {
+        eventId: eventId,
+        username: username
       }
-  });
+    });
+    if (deleted) {
+      return res.status(200).send("Attendee deleted");
+    }
+    throw new Error("Attendee not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
 
 module.exports = router;
