572a3dfe44660390bb29579f53a1061ea83095c5
Jandre' Coetzee <jandre.coetzee@entelect.co.za>
2019-10-02 11:24:22 +0200

    Migrate and seed the database on startup

--- entelectual-recognition-api/database/seeders/20190924175436-Locations.js
+++ entelectual-recognition-api/database/seeders/20190924175436-Locations.js
@@ -5,17 +5,21 @@ module.exports = {
     'Locations',
     [
       {
+        locationId: 1,
         name: 'Johannesburg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
+        locationId: 2,
         name: 'Cape Town',
         createdAt: new Date(),
         updatedAt: new Date()
       },
     ],
-    {},
+    {
+      ignoreDuplicates: true
+    },
   ),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Locations', null, {})
--- entelectual-recognition-api/database/seeders/20190924180331-Events.js
+++ entelectual-recognition-api/database/seeders/20190924180331-Events.js
@@ -5,15 +5,27 @@ module.exports = {
     'Events',
     [
       {
-        name: 'DevDay',
-        description: 'DevDay',
+        eventId: 1,
+        name: 'DevDay Johannesburg',
+        description: 'DevDay Johannesburg',
         location: 1,
         eventDate: '2019-10-10',
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        eventId: 2,
+        name: 'DevDay Cape Town',
+        description: 'DevDay Cape Town',
+        location: 2,
+        eventDate: '2019-10-03',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ],
-    {},
+    {
+      ignoreDuplicates: true
+    },
   ),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Events', null, {})
